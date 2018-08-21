1a5cc012a7e819bd8e2c25862c7abdbe90c3d2b2
Benjamin Cortes <ben@bencortes.com>
2018-08-21 12:02:22 -0400

    basic canvas image showing up

---
+++ src/rotateimage.js
@@ -0,0 +1,8 @@
+export class RotateImage {
+    constructor(){
+        this.canvas = document.getElementById('rotate');
+        this.context = this.canvas.getContext('2d');
+        this.imageObject = new Image();
+    }
+
+}
