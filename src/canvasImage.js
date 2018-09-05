e1a083e778a065a82e758bb8f5c3433d0d868cfc
Benjamin Cortes <ben@bencortes.com>
2018-09-05 14:25:14 -0400

    using class and module with no transpiled code bitches!

--- src/canvasImage.js
+++ src/canvasImage.js
@@ -4,14 +4,18 @@ export class Image {
         this.context = context;
         this.element = element;
         this.element.src = src;
-
         this.init();
     }
 
     init() {
-        this.element.onload = e => {
-            console.log(e);
-            this.context.drawImage(this.element, 1000, 2000)
+        this.element.addEventListener('load', e => console.log(e));
+        this.element.onload = (e) => {
+            console.log(e)
+            const canvasWidth = (this.canvas.width / 2);
+            const elementWidth = (this.element.width / 2);
+            const canvasHeight = (this.canvas.height / 2);
+            const elementHeight = (this.element.height / 2);
+            this.context.drawImage(this.element, canvasWidth - elementWidth, canvasHeight - elementHeight);
         }
     }
 
